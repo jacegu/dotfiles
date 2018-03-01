b2e2bfe7404432eeb2ce7daa88f503b007a06a00
Javier Acero <j4cegu@gmail.com>
2018-03-01 09:58:21 +0100

    Remove obsolete grep option

--- config/fish/exports.fish
+++ config/fish/exports.fish
@@ -20,6 +20,3 @@ set -x LESSHISTFILE /dev/null
 
 # Don’t clear the screen after quitting a manual page
 set -x MANPAGER "less -X";
-
-# Always enable colored `grep` output
-set -x GREP_OPTIONS "--color auto";
