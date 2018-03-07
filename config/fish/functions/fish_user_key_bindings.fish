4be2737303cf3b9ccbc55de635542a55bb4d0634
Javier Acero <j4cegu@gmail.com>
2018-03-07 13:40:03 +0100

    New bindings to edit command in the editor

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -5,4 +5,8 @@ function fish_user_key_bindings
   bind -M insert \cU history-search-backward
   bind -M normal \cU history-search-backward
 
+  # CTRL+e opens editor to edit the command at hand
+  bind -M insert \cE edit_command_buffer
+  bind -M normal \cE edit_command_buffer
+
 end
