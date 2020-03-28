aafdb856d760a8d7db4b26f4cad62d945aa42b28
Javier Acero <j4cegu@gmail.com>
2020-03-28 19:54:20 +0100

    Prompt update
    Mostly limiting the length the git branch will take in it. The branch
    symbol has also been removed.
    
    Also replace fish with trex.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -23,6 +23,18 @@ function _git_branch_name
   echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
 end
 
+function _short_git_branch_name
+  set -l name (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+
+  if [ (string length $name) -gt 20 ]
+   set -l prefix (string sub --length=5 $name)
+   set -l suffix (string sub --start=-15 $name)
+   echo $prefix…$suffix
+  else
+    echo $name
+  end
+end
+
 function _is_git_dirty
   echo (command git status -s --ignore-submodules=dirty ^/dev/null)
 end
@@ -31,7 +43,7 @@ function _git_branch_prompt
   set -l normal (set_color normal)
   set -l cyan (set_color cyan)
 
-  printf "$cyan%s" (_git_branch_name)
+  printf "$cyan%s" (_short_git_branch_name)
 end
 
 function _git_status_prompt
@@ -52,12 +64,12 @@ function _git_prompt
     set -l git_branch_prompt (_git_branch_prompt)
     set -l git_status_prompt (_git_status_prompt)
 
-    printf " $normal(➥ $git_branch_prompt $git_status_prompt$normal)"
+    printf " $normal($git_branch_prompt $git_status_prompt$normal)"
   end
 end
 
 function _vi_prompt
-  if test "$fish_key_bindings" = "fish_vi_key_bindings" 
+  if test "$fish_key_bindings" = "fish_vi_key_bindings"
     or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
     switch $fish_bind_mode
       case default
@@ -84,5 +96,5 @@ function fish_prompt
   set git_prompt (_git_prompt)
   set vi_prompt  (_vi_prompt)
 
-  printf "\n🐟 $normal$purple$pwd_prompt$git_prompt $black$vi_prompt ❯$normal "
+  printf "\n🦖 $normal$purple$pwd_prompt$git_prompt $black$vi_prompt ❯$normal "
 end
