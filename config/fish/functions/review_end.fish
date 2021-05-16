4781dca6b14582a113b8f8ee595313d2b9694d66
Javier Acero <j4cegu@gmail.com>
2021-05-16 19:23:01 +0200

    Make review_end aware of trunk branch

--- config/fish/functions/review_end.fish
+++ config/fish/functions/review_end.fish
@@ -1,10 +1,11 @@
 function review_end
+  set trunk (git remote show origin | grep 'HEAD' | cut -d':' -f2 | sed -e 's/^ *//g' -e 's/ *$//g')
   set current_branch (git branch | ag -Q \* | cut -f 2 -d ' ')
 
-  if [ $current_branch = "main" ]
-    echo "Already on main"
+  if [ $current_branch = $trunk ]
+    echo "Already on "$trunk
   else
-    git co main
+    git co $trunk
     git branch -D $current_branch
   end
 end
