793a9750d28e47a323b8ff9a875517dcfad44308
Javier Acero <j4cegu@gmail.com>
2021-05-16 20:50:44 +0200

    Update installation script
    Including:
    - Starting the setup with brew.
    - Considering asdf an integral part of it.

--- _setup/install.sh
+++ _setup/install.sh
@@ -66,25 +66,22 @@ function setupLinks() {
 
 
 function setLoginShell() {
-  fish_path=$(which fish);
+  FISH_PATH=`which fish`
 
-  # When fish shell is not the default shell
-  if [ "$SHELL" != $fish_path ]
+  if [ test -ef $FISH_PATH ]
   then
-    allowed_shells=$(sudo cat /etc/shells);
-
     # When fish shell is not an allowed shell: allow it
-    if [ $(echo $allowed_shells | grep -c $fish_path) -eq 0 ]
+    if [ $(sudo cat /etc/shells | grep -c "$FISH_PATH") -eq 0 ]
     then
-      sudo sh -c "echo $fish_path >> /etc/shells"
-      echo "✅ $fish_path added to /etc/shells"
+      sudo sh -c "echo $FISH_PATH >> /etc/shells"
+    , echo "✅ $FISH_PATH added to /etc/shells"
     else
-      echo "✅ $fish_path already present int /etc/shells"
+      echo "✅ $FISH_PATH already present int /etc/shells"
     fi
 
-    chsh -s `which fish` && echo "✅ $fish_path set as the login shell"
+    chsh -s `which fish` && echo "✅ $FISH_PATH set as the login shell"
   else
-    echo "✅ $fish_path is already the default shell"
+    echo "✅ $FISH_PATH is already the default shell"
   fi
 }
 
@@ -92,11 +89,11 @@ function setLoginShell() {
 function setupBrew() {
   if [ ! -f `which brew` ]
   then
-    echo "✳️  Installing Brew..."
+    echo '✳️  Installing Brew...'
     # https://brew.sh
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   else
-    echo "✅ Brew already installed"
+    echo '✅ Brew already installed'
   fi
 }
 
@@ -109,7 +106,21 @@ function setupBrewBundle() {
     echo '✅ `brew bundle` run'
   else
     echo
-    echo '✅ "Skipping `brew bundle`...'
+    echo '✅ Skipping `brew bundle`...'
+  fi
+}
+
+function setupAsdf() {
+  if [ -f `which asdf` ]
+  then
+    echo '✅ asdf already installed'
+    echo '✳️  Installing plugins...'
+    asdf plugin-add golang
+    asdf plugin-add nodejs
+    asdf plugin-add python
+    asdf plugin-add ruby
+    echo '✳️  Running `asdf install`...'
+    asdf install
   fi
 }
 
@@ -126,19 +137,21 @@ function setupVim() {
 }
 
 
-
 echo
 echo "0️⃣ Setting up dotfiles as symlinks"
 setupLinks;
 echo
-echo "1️⃣ Setting up Fish as the login shell"
-setLoginShell;
-echo
-echo "2️⃣ Setting up Brew"
+echo "1️⃣ Setting up Brew"
 setupBrew;
 setupBrewBundle;
 echo
-echo "3️⃣ Setting up Vim"
+echo "2️⃣ Setting up fish as the login shell";
+setLoginShell;
+echo
+echo "3️⃣ Setting up asdf"
+setupAsdf;
+echo
+echo "4️⃣ Setting up vim"
 setupVim;
 
 
